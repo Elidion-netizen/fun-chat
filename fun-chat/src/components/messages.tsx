31c6035a49fe90830e9c0c4973e5cb5a2fe371e7
Elidion-netizen <sapegaserg@gmail.com>
2025-04-18 08:58:19 +0300

    feat: add line to chat

--- fun-chat/src/components/messages.tsx
+++ fun-chat/src/components/messages.tsx
@@ -11,19 +11,33 @@ export function Messages({
 }): React.JSX.Element {
   return (
     <div className="flex-1 p-4 overflow-y-auto bg-white">
-      {talker !== null &&
-        `${talker.login}` in filteredMessages &&
-        filteredMessages[talker.login].read.length > 0 &&
-        filteredMessages[talker.login].read.map((el) => (
-          <MessageElement message={el} />
-        ))}
-      <br />
-      {talker !== null &&
-        `${talker.login}` in filteredMessages &&
-        filteredMessages[talker.login].unread.length > 0 &&
-        filteredMessages[talker.login].unread.map((el) => (
-          <MessageElement message={el} />
-        ))}
+      {talker !== null && `${talker.login}` in filteredMessages && (
+        <>
+          <>
+            {filteredMessages[talker.login].read.length > 0 &&
+              filteredMessages[talker.login].read.map((el) => (
+                <MessageElement message={el} />
+              ))}
+          </>
+          <>
+            {filteredMessages[talker.login].unread.length > 0 && (
+              <div className="relative my-6 flex items-center">
+                <div className="flex-grow border-t border-gray-300"></div>
+                <span className="mx-4 text-gray-500 text-sm font-medium">
+                  New Messages
+                </span>
+                <div className="flex-grow border-t border-gray-300"></div>
+              </div>
+            )}
+          </>
+          <>
+            {filteredMessages[talker.login].unread.length > 0 &&
+              filteredMessages[talker.login].unread.map((el) => (
+                <MessageElement message={el} />
+              ))}
+          </>
+        </>
+      )}
     </div>
   );
 }
